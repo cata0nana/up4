0f3a8310cd5f93caa29933f701e2258ddda8d669
cata0nana <101589752+cata0nana@users.noreply.github.com>
2023-01-21 14:31:26 -0500

    Update go_go.sh

--- src/go_go.sh
+++ src/go_go.sh
@@ -10,14 +10,12 @@ rm -rf site_5
 git reset --hard
 git pull
 cd /root/SDA_ALL/vanish_bo/ && cp /root/0nord_pass /root/SDA_ALL/vanish_bo/0nord_pass
-wget https://chromedriver.storage.googleapis.com/104.0.5112.79/chromedriver_linux64.zip && unzip -f chromedriver_linux64.zip && chmod +x chromedriver && cp chromedriver /usr/bin/
 
-chmod +x *
 pwd
 while true
 do
 	echo "NEW ..............."
 	dbus-uuidgen > /var/lib/dbus/machine-id
-	cd /root/SDA_ALL/vanish_bo/
-	timeout 5m python3 vanish_bo_indabox.py
+	cd /root/SDA_ALL/up4/
+	timeout 5m python3 van_u4u.py
 done
